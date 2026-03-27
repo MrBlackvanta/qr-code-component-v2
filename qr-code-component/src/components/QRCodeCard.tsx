859b689269e09c228805af9c1bc97b236f119e6a
Abdelrhman Ahmed <mrblackvanta@gmail.com>
2026-03-27 11:19:04 +0200

    refactor: remove unused components and files, update project structure
    - Deleted unused SVG files: favicon.svg and icons.svg.
    - Removed App component and QRCodeCard component as they were not utilized.
    - Cleaned up main.tsx and index.css files.
    - Removed TypeScript configuration files related to the app and node.
    - Updated tsconfig.app.json to include path mapping.
    - Added favicon and QR code images to assets.
    - Updated vite.config.ts to include tsconfig paths plugin for better module resolution.

--- qr-code-component/src/components/QRCodeCard.tsx
+++
@@ -1,3 +0,0 @@
-export default function QRCodeCard() {
-  return <div>QRCodeCard</div>;
-}
--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,8 @@ import { QRCodeCard } from "components";
 
 export default function App() {
   return (
-    <div>
-      <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind v4!</h1>
+    <main>
       <QRCodeCard />
-    </div>
+    </main>
   );
 }
