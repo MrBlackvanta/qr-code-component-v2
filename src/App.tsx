42cde264ec168362e761604a112fa1335477823f
Abdelrhman Ahmed <mrblackvanta@gmail.com>
2026-03-27 13:33:58 +0200

    feat: add Tailwind CSS integration, update favicon, and enhance QRCodeCard component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { QRCodeCard } from "components";
 
 export default function App() {
   return (
-    <main>
+    <main className="grid h-dvh place-content-center bg-slate-300 px-6.75">
       <QRCodeCard />
     </main>
   );
--- src/components/QRCodeCard.tsx
+++ src/components/QRCodeCard.tsx
@@ -1,3 +1,20 @@
+import qrImage from "assets/image-qr-code.png";
+
 export default function QRCodeCard() {
-  return <div>QRCodeCard</div>;
+  return (
+    <section className="flex max-w-80 flex-col gap-6 rounded-[20px] bg-white p-4 pb-10">
+      <div className="w-full max-w-72 overflow-hidden rounded-[10px]">
+        <img src={qrImage} alt="QR code" className="size-full object-cover" />
+      </div>
+      <div className="space-y-4 px-4 text-center">
+        <h1 className="text-preset-1 text-slate-900">
+          Improve your front-end skills by building projects
+        </h1>
+        <p className="text-preset-2 text-slate-500">
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </p>
+      </div>
+    </section>
+  );
 }
